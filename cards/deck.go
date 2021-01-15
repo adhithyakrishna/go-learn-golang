4d648a2a0e37aa6b0e705a5ff9f6fac80beb0a32
Adhithyakrishna <akovaisr@buffalo.edu>
2021-01-14 19:53:10 -0500

    Lesson 3 Testing

--- cards/deck.go
+++ cards/deck.go
@@ -15,8 +15,8 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
-	cardValues := []string{"Ace", "two", "three", "four"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
 		for _, val := range cardValues {
